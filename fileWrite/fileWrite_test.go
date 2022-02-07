0ae2e15dba64bdde8652b44bd3cfef3e9a623a0f
Naito TAKERU <e205720@ie.u-ryukyu.ac.jp>
2022-02-07 21:38:35 +0900

    step4 done

--- fileWrite/fileWrite_test.go
+++ fileWrite/fileWrite_test.go
@@ -3,8 +3,8 @@ package fileWrite
 import "testing"
 
 func TestFileWrite(t *testing.T) {
-    result := Hello("Yoshisaur")
-    want := "Hi, Yoshisaur. Welcome!"
+    result := Hello("takeru")
+    want := "Hi, takeru. Welcome!"
     if result != want {
         t.Errorf("fileWrite.Hello() = %q want %q", result, want)
     }
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "github.com/e205723/fileWrite/fileWrite"
+    "github.com/e205720/fileWrite/fileWrite"
 )
 
 func main() {
